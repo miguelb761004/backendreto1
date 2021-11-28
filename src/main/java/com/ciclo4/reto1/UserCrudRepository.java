996d65736d542a1117711d42b8d329685821e3c9
Miguel Bejarano <miguel.bejarano.mt@correo.usa.edu.co>
2021-11-28 13:13:17 -0500

    first commit

---
+++ src/main/java/com/ciclo4/reto1/UserCrudRepository.java
@@ -0,0 +1,11 @@
+package com.ciclo4.reto1;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserCrudRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+
+}
